c91ff20845f93b63e1163d3fa73b258afb98bbaf
Allusion001 <muhammadalispear@gmail.com>
2025-01-03 02:51:09 -0600

    Describe the changes made

--- myNotes/myNotes/settings.py
+++ myNotes/myNotes/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # STATIC_URL = '/static/'  # already declared in the default settings
 
-WHITENOISE_ROOT = './build' 
+WHITENOISE_ROOT = './build/static' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
